af6e6e2fe03c6bb6dee6236575aa0c61e21b0af1
ayden2007 <ayden.durrance@gmail.com>
2015-09-27 18:55:31 +0800

    EntireProject now shared

---
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Blog\CoreBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+/**
+ * Class PostControllerTest
+ */
+class PostControllerTest extends WebTestCase
+{
+    /**
+     * Tests post index
+     */
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
+    }
+
+}
